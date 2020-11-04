f1f4c4aced706f6fef6e7d3f20db880a63675591
Guillaume Huysmans <ghuysmans99@gmail.com>
2020-11-04 19:39:03 +0100

    Parse days in French as well

--- lib/day.ml
+++ lib/day.ml
@@ -7,14 +7,15 @@ type t =
   | Saturday
   | Sunday
 
-let of_string = function
-  | "Monday" -> Monday
-  | "Tuesday" -> Tuesday
-  | "Wednesday" -> Wednesday
-  | "Thursday" -> Thursday
-  | "Friday" -> Friday
-  | "Saturday" -> Saturday
-  | "Sunday" -> Sunday
+let of_string s =
+  match String.lowercase_ascii s with
+  | "monday" | "lundi" -> Monday
+  | "tuesday" | "mardi" -> Tuesday
+  | "wednesday" | "mercredi" -> Wednesday
+  | "thursday" | "jeudi" -> Thursday
+  | "friday" | "vendredi" -> Friday
+  | "saturday" | "samedi" -> Saturday
+  | "sunday" | "dimanche" -> Friday
   | _ -> failwith "Day.of_string"
 
 let to_string = function
