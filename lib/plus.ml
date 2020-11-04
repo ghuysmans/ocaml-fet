be5e6db05561bed526048e86fa225221aff88361
Guillaume Huysmans <ghuysmans99@gmail.com>
2020-11-04 19:43:35 +0100

    Fix empty Plus.t parsing

--- lib/plus.ml
+++ lib/plus.ml
@@ -1,8 +1,10 @@
 type 'a t = 'a list constraint 'a = _ No_plus.t
 
-let of_string s =
-  String.split_on_char '+' s |>
-  List.map No_plus.of_string
+let of_string = function
+  | "" -> []
+  | s ->
+    String.split_on_char '+' s |>
+    List.map No_plus.of_string
 
 let to_string l =
   List.map No_plus.to_string l |>
